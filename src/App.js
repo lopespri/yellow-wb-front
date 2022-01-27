027c3e94821831321d021e1bd242d2a6c1848423
Camila Nery <camila.nery@sanar.com>
2022-01-27 18:25:51 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import logo from './logo.png';
+import userIcon from './user.png';
+import premiumIcon from './premium.png';
+
+import Footer from './Footer';
 
 function App() {
+  const [u, setU] = useState();
+
+  useEffect(() => {
+    fetch('https://www.sanar-yellow.com/user').then((d) => {
+      setU(d);
+    });
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={logo} alt="" />
+        <div>O que deseja pesquisar hoje?</div>
+      </div>
+      <div style={{ padding: '40px'}}>
+        <div style={{ fontSize: '20px', fontWeight: 'bold', marginBottom: '20px' }}>Minha conta</div>
+        <div className='account-info'>
+          <div style={{ display: 'flex' }}>
+            <img src={userIcon} alt="" />
+            <div style={{ marginBottom: '20px', fontWeight: 'bold' }}>{u.name}</div>
+            <div>alicemeira@gmail.com</div>
+          </div>
+          <div>
+            <div style={{ fontWeight: 'bold', marginBottom: '15px' }}>Dados da assinatura</div>
+            <div style={{ fontWeight: 'bold', marginBottom: '10px' }}>{ u.planType ? <>Plano premium <img src={premiumIcon} /></> : <></>}</div>
+            <div style={{ fontSize: '12px', marginBottom: '10px' }}>Contratado em ${u.date}</div>
+            <div style={{ fontSize: '12px', marginBottom: '10px' }}>Valor total: R$ ${u.planCost}</div>
+          </div>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
